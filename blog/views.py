4a73c5e1814b5b6b8ed5b3a703e475a1675e6a50
kkkstp <chassse18@gmail.com>
2025-01-16 13:54:49 +0500

    #3

--- blog/views.py
+++ blog/views.py
@@ -3,19 +3,26 @@ from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
-        print(paginator.__dict__)
 
     except PageNotAnInteger:
         # Если page_number не целое число, то
@@ -27,7 +34,8 @@ def post_list(request):
         # выдать последнюю страницу
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html',
+                  {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -41,14 +49,23 @@ def post_detail(request, year, month, day, post):
     
     # Список активных комментариев к этому посту
     comments = post.comments.filter(active=True)
+
     # Форма для комментирования пользователями
     form = CommentForm()
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)
+    similar_posts = similar_posts.exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))
+    similar_posts = similar_posts.order_by('-same_tags','-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                   'comments': comments,
-                  'form': form})
+                  'form': form, 
+                  'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -110,3 +127,30 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            
+            search_vector_A = SearchVector('title', weight='A')
+            search_vector_B = SearchVector('body', weight='B')
+            search_vector = search_vector_A + search_vector_B
+            search_query = SearchQuery(query)
+            
+            results = Post.published.annotate(search=search_vector,
+                                              rank=SearchRank(search_vector, search_query)
+                                              ).filter(search=search_query).order_by('-rank')
+            
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
+
